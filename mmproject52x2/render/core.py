8721e791a50365e9f219251bcf5dbd385b08b831
Mike Boers <mikeb@markmedia.co>
2017-06-15 11:10:59 -0400

    Better sound handling.

--- mmproject52x2/render/core.py
+++ mmproject52x2/render/core.py
@@ -11,11 +11,12 @@ import subprocess
 
 from farmsoup.utils import shell_join
 from maya import cmds, mel, standalone
+from mayatools.sound import get_active_sound_node
 from mayatools.units import get_fps
 from sgfs import SGFS
 import farmsoup.client.models
 
-from mmpipeline.artifacts import to_shadow_dimension
+from mmpipeline.artifacts import to_shadow_dimension, from_shadow_dimension
 
 from . import setup as setup_module
 
@@ -39,59 +40,78 @@ def pick_out_base(scene):
     return out_dir
 
 
-def get_render_range():
+def get_render_range(sgfs=None):
     """Determine the ideal start/end frame ranges in 24fps."""
 
     fps = get_fps()
     if fps not in (12, 24):
-        raise ValueError("FPS is non-standard %s" % fps)
+        raise ValueError("FPS is non-standard {}".format(fps))
 
-    try:
-        playback_slider = mel.eval('$tmpVar = $gPlayBackSlider')
-    except RuntimeError:
-        playback_slider = None
-
-    if playback_slider:
-        sound_node = cmds.timeControl(playback_slider, query=True, sound=True)
-    else:
-        cmds.warning("Could not get gPlayBackSlider.")
-        sound_node = None
-    
-    # If we can't get the active sound, then just look for a single audio node
-    # in the file.
-    if not sound_node:
-        sound_nodes = cmds.ls(type='audio')
-        if sound_nodes:
-            if len(sound_nodes) == 1:
-                sound_node = sound_nodes[0]
-            else:
-                raise ValueError("There are multiple audio nodes.")
-
+    sound_node = get_active_sound_node()
     if sound_node:
+        print "Taking sound from active node {}.".format(sound_node)
+        sound_nodes = [sound_node]
+    else:
+        sound_nodes = cmds.ls(type='audio') or ()
 
-        offset = cmds.getAttr(sound_node + '.offset')
-        source_start = cmds.getAttr(sound_node + '.sourceStart')
-        source_end   = cmds.getAttr(sound_node + '.sourceEnd')
+    if not sound_nodes:
+        raise ValueError("There is no sound in the scene.")
 
-        # N.B. Maya's API is returning `sourceEnd` and `endFrame` as being 1 higher
-        # than the UI is reporting. There may be some different behaviour if it
-        # is in 12/24 fps (or maybe in the same/different fps as the sound was
-        # importer). Either way, we're going to let is slide as it only results
-        # in more frames being rendered, which isn't a big deal.
-
-        if source_start:
-            cmds.warning("Sound has a source start; this may mess up timing.")
-
-        start = offset
-        end = offset + source_end - source_start
+    if len(sound_nodes) == 1:
+        sound_node = sound_nodes[0]
 
     else:
-        cmds.warning("Could not get active sound node; taking timing from animation.")
-        start = cmds.playbackOptions(query=True, minTime=True)
-        end   = cmds.playbackOptions(query=True, maxTime=True)
+
+        print "Comparing audio_proxy publishes to pick best sound."
+
+        sgfs = sgfs or SGFS()
+        task = None
+        latest = None
+
+        for node in sound_nodes:
+            
+            path = cmds.sound(node, q=True, file=True)
+            
+            publish = sgfs.entities_from_path(path, 'PublishEvent')
+            if not publish:
+                raise ValueError("Sound file '{}' from node {} is not a publish, so we can't tell them apart.".format(path, node))
+            
+            # We're looking at created_at instead of version, because I want
+            # versions to be less meaningful in the future.
+            type_, this_task, created_at = publish.fetch('sg_type', 'link', 'created_at')
+
+            if type_ != 'audio_proxy':
+                raise ValueError("Sound file '{}' from node {} is not an 'audio_proxy' publish.".format(path, node))
+
+            if task and task is not this_task:
+                raise ValueError("Sound files are from different tasks: {} and {}.".format(task, this_task))
+            task = task or this_task
+
+            if not latest or created_at > latest:
+                sound_node = node
+                latest = created_at
+
+    print "Taking timing from sound file '{}' from node {}.".format(cmds.sound(sound_node, q=True, file=True), sound_node)
+
+
+    offset = cmds.getAttr(sound_node + '.offset')
+    source_start = cmds.getAttr(sound_node + '.sourceStart')
+    source_end   = cmds.getAttr(sound_node + '.sourceEnd')
+
+    # N.B. Maya's API is returning `sourceEnd` and `endFrame` as being 1 higher
+    # than the UI is reporting. There may be some different behaviour if it
+    # is in 12/24 fps (or maybe in the same/different fps as the sound was
+    # importer). Either way, we're going to let is slide as it only results
+    # in more frames being rendered, which isn't a big deal.
+
+    if source_start:
+        cmds.warning("Sound has a source start; this may mess up timing.")
+
+    start = offset
+    end = offset + source_end - source_start
 
     if start and float(start) != 1.0:
-        cmds.warning('Start is %s; resetting to 1.' % start)
+        cmds.warning('Sound sourceStart is %s; resetting to 1.' % start)
         start = 1.0
 
     # We leave start where it is, because Kevin has request that we start
@@ -105,7 +125,7 @@ def get_render_range():
 
 
 
-def submit(scene, start=None, end=None, out_base=None, out_dir=None, name=None, shadow_scene=None, **kwargs):
+def submit(scene, start=None, end=None, out_base=None, out_dir=None, name=None, shadow_scene=None, verbose=True, dry_run=False, **kwargs):
 
     if start is None or end is None:
 
@@ -135,20 +155,22 @@ def submit(scene, start=None, end=None, out_base=None, out_dir=None, name=None, 
     setup_command = [
         #'vee', 'exec', '--bootstrap', # FIXME: For dev mode.
         'mm52x2-render', '--setup',
-        '-s', str(start),
-        '-e', str(end),
-        '-O', out_dir,
-        '-n', name,
+        '--start', str(start),
+        '--end', str(end),
+        '--out-dir', out_dir,
+        '--name', name,
         '--shadow-scene', shadow_scene,
         scene,
     ]
-    # TODO: Remove hostname when farmsoup GUI scan identify the worker.
-    setup_command = 'hostname; ' + shell_join(setup_command)
+    setup_command = shell_join(setup_command)
+    if verbose:
+        print 'setup command:', setup_command
     setup_job = farmsoup.client.models.Job(
         name='Render setup.',
         label='setup',
-    ).setup_subprocess(
-        [setup_command],
+    ).setup_as_subprocess(
+        # TODO: Remove hostname when farmsoup GUI scan identify the worker.
+        ['hostname; ' + setup_command],
         #extra_env={'VEE_EXEC_ARGS': os.environ.get('VEE_EXEC_ARGS', '')}, # FIXME.
         shell=True,
     )
@@ -179,34 +201,37 @@ def submit(scene, start=None, end=None, out_base=None, out_dir=None, name=None, 
         shadow_scene,
     ]
     # HACK: Get around escaping the variables.
-    render_command = 'hostname; ' + shell_join(render_command).replace('__F', '$F')
+    render_command = shell_join(render_command).replace('__F', '$F')
+    if verbose:
+        print 'render command:', render_command
+
     render_job = farmsoup.client.models.Job(
         name='Render.',
         label='render',
         requirements='jobs["setup"].status == "complete"',
-    ).setup_subprocess(
-        [render_command],
+    ).setup_as_subprocess(
+        ['hostname; ' + render_command],
         shell=True,
     )
     render_job.expand_via_range('F=%d-%d/10' % (start, end))
 
-    compress_job = farmsoup.client.models.Job(
-        name='Compress.',
-        label='compress',
-        requirements='jobs["render"].status == "complete"',
-    ).setup_subprocess(
-        ['echo', u'Good job everyone!'],
-        shell=True,
-    )
+    # compress_job = farmsoup.client.models.Job(
+    #     name='Compress.',
+    #     label='compress',
+    #     requirements='jobs["render"].status == "complete"',
+    # ).setup_as_subprocess(
+    #     ['echo', u'Good job everyone!'],
+    #     shell=True,
+    # )
 
-    client = farmsoup.client.Client()
-    group = client.submit(
-        id=int(os.environ.get('FARMSOUP_JOB_GROUP_ID', '0')) or None,
-        name='mm52x2-render :: %s' % name,
-        jobs=[setup_job, render_job, compress_job],
-    )
-
-    print 'Submitted as', group.id
+    if not dry_run:
+        client = farmsoup.client.Client()
+        group = client.submit(
+            id=int(os.environ.get('FARMSOUP_JOB_GROUP_ID', '0')) or None,
+            name='mm52x2-render :: %s' % name,
+            jobs=[setup_job, render_job],
+        )
+        print 'group id:', group.id
 
 
 def setup(scene, shadow_scene, **kwargs):
@@ -241,7 +266,7 @@ def main():
         help="Base output directory.")
     global_args.add_argument('-O', '--out-dir',
         help="Actual output directory.")
-    global_args.add_argument('-n', '--name',
+    global_args.add_argument('-n', '--name', # TODO: Rename this to -N when they are off the farm.
         help="Name of output within directory.")
 
     setup_args = parser.add_argument_group('Render setup options')
@@ -249,6 +274,10 @@ def main():
         help='Run render setup, and save the scene to the shadow dimension.')
     setup_args.add_argument('--shadow-scene',
         help='Where to save the setup scene.')
+
+    runtime_args = parser.add_argument_group('Runtime options')
+    runtime_args.add_argument('-v', '--verbose', action='store_true')
+    runtime_args.add_argument('--dry-run', action='store_true')
 
     parser.add_argument('scene')
     args = parser.parse_args()
